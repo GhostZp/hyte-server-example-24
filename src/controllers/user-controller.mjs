d29761598a4c3ee3de158c386d056ccf4e84c7d0
GhostZp <amsku.kallio@gmail.com>
2024-03-02 11:23:08 +0200

    Edited validation code

--- src/controllers/user-controller.mjs
+++ src/controllers/user-controller.mjs
@@ -24,12 +24,10 @@ const getUserById = async (req, res) => {
   return res.json(result);
 };
 
-const postUser = async (req, res) => {
+const postUser = async (req, res, next) => {
   const {username, password, email} = req.body;
-  // validation errors can be retrieved from the request object (added by express-validator middleware)
   const validationErrors = validationResult(req);
-  // check if any validation errors
-  console.log('User validation error');
+  console.log('user validation errors', validationErrors);
   // check that all needed fields are included in request
   if (validationErrors.isEmpty()) {
     const salt = await bcrypt.genSalt(10);
@@ -39,10 +37,6 @@ const postUser = async (req, res) => {
       email,
       password: hashedPassword,
     }, next);
-/*     if (result.error) {
-      const error = new Error('');
-      return res.status(result.error).json(result);
-    } */
     return res.status(201).json(result);
   } else {
     const error = new Error('bad request');
@@ -55,15 +49,15 @@ const postUser = async (req, res) => {
 // Only user authenticated by token can update own data
 const putUser = async (req, res) => {
   // Get userinfo from req.user object extracted from token
-  const user_id = req.user.user_id;
+  const userId = req.user.user_id;
   const {username, password, email} = req.body;
   // hash password if included in request
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
   // check that all needed fields are included in request
-  if (user_id && username && password && email) {
+  if (userId && username && password && email) {
     const result = await updateUserById({
-      user_id,
+      userId,
       username,
       password: hashedPassword,
       email,
